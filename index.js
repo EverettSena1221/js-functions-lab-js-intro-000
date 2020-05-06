53f57881a47233d76a5427c25f9c8bfa762ff86b
Evaristo Sena <everett.sena79@gmail.com>
2020-05-06 08:07:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ function happyHolidaysTo(you) {
 function happyCustomHolidayTo(Halloween, you) {
   return (`Happy ${Halloween}, ${you}!`)
 }
-function holidayCountdown(Halloween, days) {
-  return (`It's ${days} until ${Halloween}!`)
+function holidayCountdown(Mother\'s Day, 20) {
+  return (`It's ${20} until ${Mother\'s Day}!`)
 }
