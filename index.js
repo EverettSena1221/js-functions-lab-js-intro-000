7cd1f8f0310b50190f73f4d6c640713e57a1a733
Evaristo Sena <everett.sena79@gmail.com>
2020-05-06 07:13:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,6 +8,3 @@ function happyHolidaysTo(you) {
 function happyCustomHolidayTo(Halloween, you) {
   return (`Happy ${Halloween}, ${you}!`)
 }
-function holidayCountdown (Christmas, 233) {
-  return (`It's ${233} days until ${Christmas}!`);
-}
